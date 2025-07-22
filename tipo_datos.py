6454881671aa4dbd0e285e3e6351bf04398ec4a2
Roy-Garcia-Rendon <rodrigoa.gr11@gmail.com>
2025-07-21 18:21:45 -0600

    proyecto de pruba

---
+++ tipo_datos.py
@@ -0,0 +1,6 @@
+edad = 24
+estatura = 1.80
+estaturaa = True
+print(f"Hola, tengo {edad} años y mido {estatura} metros.")
+print(f"hola, el proximo año tendre {edad + 1}")
+print("mides más de 1.71:", estaturaa)
---
+++ variables.py
@@ -0,0 +1,5 @@
+nombre = "Rodrigo"
+edad = 25
+m = "Mexico"
+
+print(f"hola, me llamo {nombre}, tengo {edad + 1}, vivo en {m}")
