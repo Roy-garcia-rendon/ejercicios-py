3529d3143f3ab50d1f9eb1592b88743141e1bc7f
Roy-Garcia-Rendon <rodrigoa.gr11@gmail.com>
2025-07-31 22:43:06 -0600

    for y while

---
+++ for_while.py
@@ -0,0 +1,12 @@
+frutas = ["manzana", "pera", "uva"]
+for fruta in frutas:
+    print("hola", fruta)
+
+#Repetir algo con Range()
+#for i in range(2):
+#   print(i)    
+
+contador = 0
+while contador <= 5:
+    print(contador)
+    contador += 1
