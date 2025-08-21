18ddd8d5bf1de5117cc548e766c3b5f524aecf8b
Roy-Garcia-Rendon <rodrigoa.gr11@gmail.com>
2025-08-20 20:26:43 -0600

    def ejercicios

---
+++ src/exercises/de3.py
@@ -0,0 +1,5 @@
+def por():
+    i = int(input("Ingresa primer número: "))
+    j = int(input("Ingresa segundo número: "))
+    return i * j
+print(por())
---
+++ src/exercises/def1.py
@@ -0,0 +1,7 @@
+def p():
+    i = int(input("Ingresa un número: "))
+    if i % 2 == 0:
+        print ("Es par")
+    else:
+        print("Es impar")
+p()
