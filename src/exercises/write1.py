cfe88520196635aeea13851f8567acb519258417
Roy-Garcia-Rendon <rodrigoa.gr11@gmail.com>
2025-08-25 20:48:49 -0600

    eje write,input, listas

--- src/exercises/write1.py
+++ src/exercises/write1.py
@@ -1,3 +1,21 @@
-with open("agenta.txt", "a") as archivo:
-    texto = input("Ingresa un texto: ")
-    archivo.write(texto + "\n")
+while True:
+    print("1. Sobre escribir archivo")
+    print("2. Agregar al archivo")
+    print("3. Salir")
+
+    opcion = input("Ingresa opción: ")
+    if opcion == "1":
+        with open("notas.txt", "w") as archivo:
+            nota = input("Escribe tu nota:")
+            archivo.write(nota + "\n")
+        print("Archivo sobrescrito")
+    elif opcion == "2":
+        with open("notas.txt", "a") as archivo:
+            nota = input("Escribe tu nota: ")
+            archivo.write(nota + "\n")
+        print("Nota agregada con exito")
+
+    elif opcion == "3":
+        break
+    else:
+        print("Opción invalida")
